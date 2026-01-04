bb70bd3b3c1a0fc9b623ea297ffacba0626651d6
Prabin Tiwari <Prabintiwari964@gmail.com>
2026-01-04 22:20:37 +0545

    feat: Add OTP verification and password reset routes with Swagger documentation

--- Backend/src/routes/user.routes.ts
+++ Backend/src/routes/user.routes.ts
@@ -42,20 +42,81 @@ const router = Router();
 router.patch(
   "/users/update-profile/:id",
   cloudinaryUpload("users/profile").single("profileImage"),
-  validate(updateUserSchema),
-  updateUserDetails
-);
-router.patch(
-  "/update-profile/:id",
   authenticateToken,
-  cloudinaryUpload("users/profile").single("profileImage"),
   validate(updateUserSchema),
   updateUserDetails
 );
 
+/**
+ * @swagger
+ * /api/users/forgot-password:
+ *   post:
+ *     summary: Forgot password 
+ *     tags: [User]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Forgot password sent successfully
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: User not found
+ */
 router.post("/forgot-password", forgotPassword);
+
+/**
+ * @swagger
+ * /api/users/resend-otp:
+ *   post:
+ *     summary: Resend Otp
+ *     tags: [User]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Otp resend successfully
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: User not found
+ */
 router.post("/resend-otp", resendOtp);
+
+/**
+ * @swagger
+ * /api/users/verify-otp:
+ *   post:
+ *     summary: verify Otp
+ *     tags: [User]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Otp verify successfully
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: User not found
+ */
 router.post("/verify-otp", verifyOtp);
+
+/**
+ * @swagger
+ * /api/users/reset-password:
+ *   post:
+ *     summary: Reset password
+ *     tags: [User]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Password reset successfully
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: User not found
+ */
 router.post("/reset-password", resetPassword);
 
 export default router;
